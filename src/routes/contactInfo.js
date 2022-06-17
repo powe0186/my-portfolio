f4fe40d91f3c89473bd0ac54a7266aab961541ae
Ben Powell <powe0186@gmail.com>
2022-06-17 15:51:05 -0500

    Added links to contact info page

--- src/routes/contactInfo.js
+++ src/routes/contactInfo.js
@@ -9,10 +9,10 @@ const ContactInfo = () => {
 
     return (
         <ListGroup style={ { margin: "5rem" }}>
-            <ListGroup.Item style={ contactInfoStyle }>Email: powe0186@gmail.com</ListGroup.Item>
+            <ListGroup.Item style={ contactInfoStyle }>Email: <a href="mailto:powe0186@gmail.com">powe0186@gmail.com</a></ListGroup.Item>
             <ListGroup.Item style={ contactInfoStyle }>Phone: 651-324-3241</ListGroup.Item>
-            <ListGroup.Item style={ contactInfoStyle }>GitHub: https://github.com/powe0186</ListGroup.Item>
-            <ListGroup.Item style={ contactInfoStyle }>LinkedIn: https://www.linkedin.com/in/benjamin-powell10/</ListGroup.Item>
+            <ListGroup.Item style={ contactInfoStyle }>GitHub: <a href="https://github.com/powe0186">https://github.com/powe0186</a></ListGroup.Item>
+            <ListGroup.Item style={ contactInfoStyle }>LinkedIn: <a href="https://www.linkedin.com/in/benjamin-powell10/">https://www.linkedin.com/in/benjamin-powell10/</a></ListGroup.Item>
         </ListGroup>
     )
 }
